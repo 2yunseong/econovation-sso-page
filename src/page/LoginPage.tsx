bd183c125eeca2d2e25ee986ecf501fbce526cf7
2yunseong <dbsdltjd3701@naver.com>
2023-06-13 21:42:46 +0900

    feat: 로그인 페이지 레이아웃 구현

---
+++ src/page/LoginPage.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import { PageLayout } from '../style/components/Layout';
+
+const LoginPage = () => {
+  return <PageLayout></PageLayout>;
+};
+
+export default LoginPage;
