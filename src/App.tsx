45bd522c36c5ce34d76422c190223b05267e5778
2yunseong <dbsdltjd3701@naver.com>
2023-06-15 15:51:07 +0900

    feat: 리셋 스타일 제거

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,18 @@ import React from 'react';
 import { Routes, Route, BrowserRouter } from 'react-router-dom';
 import { RecoilRoot } from 'recoil';
 import SignUpPage from './page/SignUpPage';
+import ResetStyle from './style/global/ResetStyle';
 
 function App() {
   return (
     <BrowserRouter>
-      <RecoilRoot>
-        <Routes>
-          <Route path="/signup" element={<SignUpPage />} />
-        </Routes>
-      </RecoilRoot>
+      <ResetStyle>
+        <RecoilRoot>
+          <Routes>
+            <Route path="/signup" element={<SignUpPage />} />
+          </Routes>
+        </RecoilRoot>
+      </ResetStyle>
     </BrowserRouter>
   );
 }
