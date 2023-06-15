928252d6f9ed11717ed561e22defea7ed78680a3
2yunseong <dbsdltjd3701@naver.com>
2023-06-15 16:58:11 +0900

    fix: 잘못된 컴포넌트 사용 수정
    - resetStyle 단일 렌더링

--- src/App.tsx
+++ src/App.tsx
@@ -3,18 +3,19 @@ import { Routes, Route, BrowserRouter } from 'react-router-dom';
 import { RecoilRoot } from 'recoil';
 import SignUpPage from './page/SignUpPage';
 import ResetStyle from './style/global/ResetStyle';
+import LoginPage from './page/LoginPage';
 
 function App() {
   return (
-    <BrowserRouter>
-      <ResetStyle>
-        <RecoilRoot>
-          <Routes>
-            <Route path="/signup" element={<SignUpPage />} />
-          </Routes>
-        </RecoilRoot>
-      </ResetStyle>
-    </BrowserRouter>
+    <RecoilRoot>
+      <ResetStyle />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<LoginPage />} />
+          <Route path="/signup" element={<SignUpPage />} />
+        </Routes>
+      </BrowserRouter>
+    </RecoilRoot>
   );
 }
 
