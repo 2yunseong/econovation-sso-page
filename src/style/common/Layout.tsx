21ace6fe5c0b9683cfd1c647a6096b0446d9c3ec
2yunseong <dbsdltjd3701@naver.com>
2023-06-15 16:59:09 +0900

    refactor: 추상적인 네이밍으로 변경

---
+++ src/style/common/Layout.tsx
@@ -0,0 +1,18 @@
+import { styled } from 'styled-components';
+
+export const Layout = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+export const Title = styled.h1`
+  font-size: 40px;
+  font-weight: 500;
+`;
+
+export const SubTitle = styled.h2`
+  font-size: 20px;
+  font-weight: 500;
+  color: #969696;
+`;
