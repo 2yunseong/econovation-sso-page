a24a3c1480fe4ff1733ef43dd05df5b93e79300f
2yunseong <dbsdltjd3701@naver.com>
2023-06-16 14:43:02 +0900

    feat: 에러 메세지 컴포넌트 스타일링

--- src/components/common/ErrorBox.style.tsx
+++ src/components/common/ErrorBox.style.tsx
@@ -1,6 +1,18 @@
-import { styled } from 'styled-components';
+import { keyframes, styled } from 'styled-components';
+
+const slideIn = keyframes`
+  0% {
+    transform: translateY(-100%);
+    opacity: 0;
+  }
+  100% {
+    transform: translateY(0);
+    opacity: 1;
+  }
+`;
 
 export const Label = styled.p`
   font-size: 20px;
   color: #dc0000;
+  animation: ${slideIn} 0.4s ease-out;
 `;
